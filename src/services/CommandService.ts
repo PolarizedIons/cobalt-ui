3563b2f6f11734a93fea2e217521827a3fce287b
PolarizedIons <polarizedions@gmail.com>
2020-01-08 22:57:35 +0200

    added service base

--- src/services/CommandService.ts
+++ src/services/CommandService.ts
@@ -1,12 +1,9 @@
-import axios from "axios";
 import ICommand from "@/types/ICommand";
 import { ApiResponse } from "../types/ApiResponse";
+import ServiceBase from "./ServiceBase";
 
-const baseURL =
-    window.location.hostname === "localhost" ? "http://localhost:3000" : "";
-
-export default class CommandService {
+export default class CommandService extends ServiceBase {
     static listCommands(): Promise<ApiResponse<ICommand[]>> {
-        return axios.get(`${baseURL}/v1/commands`).then(resp => resp.data);
+        return this.client.get("/v1/commands").then(resp => resp.data);
     }
 }

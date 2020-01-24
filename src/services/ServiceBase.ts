7bd34edf200611d2e81236ba42936f1bec2de032
PolarizedIons <polarizedions@gmail.com>
2020-01-24 18:34:37 +0200

    set it correctly

--- src/services/ServiceBase.ts
+++ src/services/ServiceBase.ts
@@ -2,13 +2,13 @@ import axios from "axios";
 import { ApiResponse } from "@/types/ApiResponse";
 
 export default class ServiceBase {
-  public static baseURL =
+  public static baseDomain =
     window.location.hostname === "localhost" || !process.env.VUE_APP_API_DOMAIN
       ? "localhost"
       : process.env.VUE_APP_API_DOMAIN;
 
   public static client = axios.create({
-    baseURL: ServiceBase.baseURL,
+    baseURL: window.location.protocol + "//" + ServiceBase.baseDomain,
     responseType: "json",
     headers: {
       "Content-Type": "application/json"
